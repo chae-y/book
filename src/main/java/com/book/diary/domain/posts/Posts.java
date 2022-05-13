50c511e19b88307f4871be9c482fbc47bfd11a3a
chae <stat_chae0@sookmyung.ac.kr>
2022-05-14 00:34:13 +0900

    프로젝트에 spring data jpa 적용하기

---
+++ src/main/java/com/book/diary/domain/posts/Posts.java
@@ -0,0 +1,32 @@
+package com.book.diary.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor //기본 생성자 자동 추가
+@Entity // 테이블과 링크될 클래스임을 나타냄, 기본값으로 클래스의 카멜케이스 이름을 언더스코어 네이밍으로 테이블 이름을 매칭함
+public class Posts {
+
+    @Id//해당 테이블의 PK(primary key)필드를 나타냄냄
+   @GeneratedValue(strategy = GenerationType.IDENTITY)//pk 생성규측을 나타냄 generationtype옵션 추가해야 auto_increment
+    private Long Id;
+
+    @Column(length = 500, nullable = false)//테이블 칼럼을 나타내며 굳이 선언하지 않더라도 해당 클래스의 필드는 모두 칼럼, 길이 늘리고 싶거나 그럴때 사용
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private  String content;
+
+    private String author;
+
+    @Builder // 해당 클래스의 빌더 패턴 클래스를 생성, 생성자 상다ㄴ에 선언 시 생성자에 포함된 필드만 빌더에 포함
+    public Posts(String title, String content, String author){
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
