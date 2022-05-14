1702379321128743d538e2df82b56d1b485ad6ac
chae <stat_chae0@sookmyung.ac.kr>
2022-05-14 16:41:37 +0900

    spring data jpa 테스트코드 작성하기

---
+++ src/test/java/com/book/diary/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,46 @@
+package com.book.diary.domain.posts;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest //별다른 설정없이 사용하면 h2를 자동으로 실행 해줌
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After
+    public void cleanup(){
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기(){
+        //given
+        String title = "테스트 게시글";
+        String content = "content";
+
+        postsRepository.save(Posts.builder() // 테이블 posts에 insert/update 쿼리 실행
+                                .title(title)
+                                .content(content)
+                                .author("jojo@gmail.com")
+                                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+}
